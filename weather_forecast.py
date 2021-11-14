83d04df72f23dc7244d4f0b94afd69b07d10fcaf
Nikhilesh Shah <nikhileshshah10@gmail.com>
2021-11-14 14:48:00 +0530

    Update weather_forecast.py

--- weather_forecast.py
+++ weather_forecast.py
@@ -114,16 +114,12 @@ if(st.button("SUBMIT")):
         else:
             temp=str(round((((x["main"]["temp"]-cel)*1.8)+32),2))
         
-        col1, col2 = st.expander(2)
-        with col1:
-            st.write("## Current Temperature ")
-        with col2:
-            st.image(f"http://openweathermap.org/img/wn/{icon}@2x.png",width=70)
+        st.write("## Current Temperature ")
+        st.image(f"http://openweathermap.org/img/wn/{icon}@2x.png",width=70)
 
         
-        col1, col2= st.expander(2)
-        col1.metric("TEMPERATURE",temp+temp_unit)
-        col2.metric("WEATHER",current_weather)
+        st.metric("TEMPERATURE",temp+temp_unit)
+        st.metric("WEATHER",current_weather)
         st.subheader(" ")
         
         if graph=="Bar Graph":
