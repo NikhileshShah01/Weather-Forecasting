0516fa4e32e7b57adf5c386887ca803bc75bfdda
Nikhilesh Shah <nikhileshshah10@gmail.com>
2021-12-03 15:00:06 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime,requests
 import plotly.graph_objects as go
 
-st.title("8-DAY WEATHER FORECAST 🌏🌧️🌪️")
+st.title("8-DAY WEATHER FORECAST 🌎🌧️")
 
 city=st.text_input("ENTER THE NAME OF THE CITY ")
 
@@ -159,7 +159,7 @@ if(st.button("SUBMIT")):
         st.header(' ')
         st.header(' ')
         st.markdown(" Made with :heart:")
-        st.markdown("by Nikhilesh Shah,Nandita Agarwal and Nisha Vaghela 🤩🤩")
+        st.markdown("by Nikhilesh Shah 🤩,Nandita Agarwal 🤗 and Nisha Vaghela 🥰")
     except KeyError:
         st.error(" Invalid city!!  Please try again !!")
 
