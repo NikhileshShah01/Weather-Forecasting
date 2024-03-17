b1d0e9ea7fb863eadc88945293d30a93ef4380ee
Nikhilesh Shah <nikhileshshah10@gmail.com>
2024-03-17 11:57:37 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 import datetime,requests
 from plotly import graph_objects as go
 
@@ -14,17 +15,28 @@ speed=st.selectbox("SELECT WIND SPEED UNIT ",["Metre/sec","Kilometre/hour"])
 
 graph=st.radio("SELECT GRAPH TYPE ",["Bar Graph","Line Graph"])
 
-st.markdown(
-    """
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
     <style>
-    .reportview-container {
-        background: url("/images/bgsky.png")
+	.reportview-container {
+        background: url("data:image/png;base64,%s");
     }
-  
     </style>
-    """,
-    unsafe_allow_html=True
-)
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('/images/bgsky.png')
 
 if unit=="Celsius":
     temp_unit=" °C"
